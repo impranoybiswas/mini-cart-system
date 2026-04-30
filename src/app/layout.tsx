eb3bc8edc78242e9bfa20706afe36cf728309ed2
Pranoy Biswas Bappa <impranoybiswas@gmail.com>
2026-04-30 10:01:59 +0600

    products and type create

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
-import StoreProvider from "./StoreProvider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>
-        <StoreProvider>{children}</StoreProvider>
+        {children}
       </body>
     </html>
   );
